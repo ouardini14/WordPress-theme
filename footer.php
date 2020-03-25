79d3794b0d569b702e8198a8692a04254a5d0e1a
said wld lhwat <lharoui.elmehdi@gmail.com>
2020-03-25 02:15:11 +0100

    modification footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <div id="footer">
-          &copy 2014 yourdomain.com | All Rights Reserved |  <a href="http://binarytheme.com" style="color: #fff" target="_blank">Design by : binarytheme.com</a>
+          école supérieure de technologir sidi bennour
     </div>
      <!-- FOOTER SECTION END-->
    
